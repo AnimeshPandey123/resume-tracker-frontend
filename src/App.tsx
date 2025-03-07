3fee268bf7f5a6e953f049dae0762fce2305c2d6
Animesh Pandey <ani.mesh13578@gmail.com>
2025-03-07 13:48:54 +0000

    feat: converted to typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Resumes from '@/components/Resumes'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Resumes />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Resumes.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import "@/styles/resumes.css"
+// import API_URL from '@/config'
+
+interface Resume {
+  id: number;
+  title: string;
+  summary: string;
+}
+
+function Resumes() {
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const API_URL = import.meta.env.VITE_API_URL ;
+
+
+  useEffect(() => {
+    resumesFetch();
+  }, []);
+
+  const resumesFetch = async (): Promise<void> => {
+    try {
+      const url = API_URL + '/api/resumes?user_id=1'
+      const response = await fetch(url);
+      const data: Resume[] = await response.json();
+      setResumes(data);
+    } catch (error) {
+      console.error('Error fetching resumes:', error);
+    }
+  };
+
+  return (
+    <div className="Resumes">
+      <div>Resumes:</div>
+      <div className="resumes">
+        {resumes.map((resume) => (
+          <div className="resume-item" key={resume.id}>
+            {resume.title}: {resume.summary}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Resumes;
